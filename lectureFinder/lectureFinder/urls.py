d30b17f6873401f9712637c65fd67d84304acee7
deden3791 <99360322+deden3791@users.noreply.github.com>
2022-03-14 10:10:37 +0000

    Update urls.py

--- lectureFinder/lectureFinder/urls.py
+++ lectureFinder/lectureFinder/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
