950598379fae743f0b3d2066b0caeb858bb4ddba
deden3791 <aeden3791@gmail.com>
2022-03-14 10:48:21 +0000

    mapped URLs

--- lectureFinder/lectureFinderApp/urls.py
+++ lectureFinder/lectureFinderApp/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from django.conf.urls import url
-from rango import views
+from lectureFinderApp import views
 
 app_name = 'lectureFinderApp'
 
 urlpatterns = [ 
 	path('', views.index, name='index'),
-	path('about/', views.about, name='about'),
-	path('register/', views.register, name='register'),
-	path('login/', views.user_login, name='login'),
-	path('restricted/', views.restricted, name='restricted'),
-	path('logout/', views.user_logout, name='logout'),
+	path('members/', views.members, name='members'),
+	path('search/', views.search, name='search'),
+	#path('about/', views.about, name='about'),
+	#path('register/', views.register, name='register'),
+	#path('login/', views.user_login, name='login'),
+	#path('restricted/', views.restricted, name='restricted'),
+	#path('logout/', views.user_logout, name='logout'),
 ]
--- lectureFinder/lectureFinderApp/views.py
+++ lectureFinder/lectureFinderApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	return render(request, 'lectureFinderApp/index.html')
+
+def members(request):
+	return render(request, 'lectureFinderApp/members.html')
+
+def search(request):
+	return render(request, 'lectureFinderApp/search.html')
