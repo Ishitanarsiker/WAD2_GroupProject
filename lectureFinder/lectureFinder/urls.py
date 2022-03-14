c0da63fd0161174070b7598d03a15b17fbcb9d80
deden3791 <99360322+deden3791@users.noreply.github.com>
2022-03-14 10:18:34 +0000

    Update urls.py

--- lectureFinder/lectureFinder/urls.py
+++ lectureFinder/lectureFinder/urls.py
@@ -22,6 +22,6 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('lectureFinderApp.urls')),
+    path('lectureFinderApp/', include('lectureFinderApp.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
