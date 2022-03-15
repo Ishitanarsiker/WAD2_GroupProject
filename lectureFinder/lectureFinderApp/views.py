d0ea4f40779a82e21e29b46e9c98fd2acadf98a7
deden3791 <aeden3791@gmail.com>
2022-03-15 15:34:05 +0000

    Fixing Templates

--- lectureFinder/lectureFinderApp/views.py
+++ lectureFinder/lectureFinderApp/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'lectureFinderApp/index.html')
 
+def about(request):
+	return render(request, 'lectureFinderApp/about.html')
+
 def members(request):
 	return render(request, 'lectureFinderApp/members.html')
 
