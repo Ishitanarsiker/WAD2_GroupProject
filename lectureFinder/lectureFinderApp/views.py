c78477397560cef96afe6706b8d201db62fe4af1
= <=>
2022-03-15 16:05:32 +0000

    Updated URLS and Index view

--- lectureFinder/lectureFinderApp/views.py
+++ lectureFinder/lectureFinderApp/views.py
@@ -1,10 +1,31 @@
 from django.shortcuts import render
+from lectureFinderApp.models import Lecture
+
 
 def index(request):
-	return render(request, 'lectureFinderApp/index.html')
+	# Index page will display both the recently uploaded and the most viewed lectures.
+	all_lectures = Lecture.objects.all()  # ordered by last record inserted into the table.
+	all_lectures_by_likes = all_lectures.order_by('-likes')
+
+	context_dict = {
+		'recently_uploaded': all_lectures,
+		'most_viewed': all_lectures_by_likes
+	}
+
+	return render(request, 'lectureFinderApp/index.html', context=context_dict)
+
 
 def members(request):
 	return render(request, 'lectureFinderApp/members.html')
 
+
 def search(request):
 	return render(request, 'lectureFinderApp/search.html')
+
+
+def about(request):
+	return render(request, 'lectureFinderApp/about.html')
+
+
+def show_lecture(request, lecture_id):
+	return render(request, 'lectureFinderApp/show_lecture.html')
