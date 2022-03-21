94841c5c70f3f9cca9733d46cdd67a7e014aef67
2542408M <2542408M@student.gla.ac.uk>
2022-03-21 14:00:00 +0000

    Remove unused forms.py code.

--- lectureFinder/lectureFinderApp/forms.py
+++ lectureFinder/lectureFinderApp/forms.py
@@ -2,25 +2,6 @@ from django import forms
 from .models import UserProfile, Lecture
 from django.contrib.auth.models import User
 
-# class CourseForm(forms.Form):
-# 	class Meta:
-#         model = Course
-#         fields = ('name', 'code', 'user')
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields['user'].queryset = User.objects.none()
-#
-#         if 'name' in self.data:
-#             try:
-#                 country_id = int(self.data.get('name'))
-#                 self.fields['user'].queryset = User.objects.filter(name_id=name_id).order_by('name')
-#             except (ValueError, TypeError):
-#                 pass  # invalid input from the client; ignore and fallback to empty City queryset
-#         elif self.instance.pk:
-#             self.fields['user'].queryset = self.instance.country.user_set.order_by('name')
-#
-
 
 class UploadLectureForm(forms.ModelForm):
     class Meta:
