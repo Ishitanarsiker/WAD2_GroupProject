a19e9143c7aa62d3da3a01654174082631af8427
2572157h <97886534+2572157h@users.noreply.github.com>
2022-03-22 18:56:41 +0000

    add password reset

--- lectureFinder/lectureFinderApp/urls.py
+++ lectureFinder/lectureFinderApp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from lectureFinderApp import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'lectureFinderApp'
 
@@ -15,5 +16,9 @@ urlpatterns = [
 	path('signup/', views.signup, name='signup'),
 	path('login/', views.login_user, name='login'),
 	path('logout/', views.logout_user, name='logout'),
+	path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
