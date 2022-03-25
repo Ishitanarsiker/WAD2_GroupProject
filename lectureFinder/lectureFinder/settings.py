912d6a494beb83cbcac1e5c3a6e38ebd6c4ec4d7
2542408M <2542408M@student.gla.ac.uk>
2022-03-25 18:53:58 +0000

    Update settings.py to work with pythonanywhere.

--- lectureFinder/lectureFinder/settings.py
+++ lectureFinder/lectureFinder/settings.py
@@ -41,9 +41,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret('SECRET_KEY', secrets)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['lukemullenuofg.pythonanywhere.com']
+ALLOWED_HOSTS = ['lukemullenuofg.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
