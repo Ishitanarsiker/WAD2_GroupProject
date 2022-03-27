5526d760dc8c42d2e8c3c37c2010c9efe08d6285
Petros Kitazos <2526547k@student.gla.ac.uk>
2022-03-27 19:51:08 +0100

    added Upload files tab and modified upload form

--- lectureFinder/static/js/members.js
+++ lectureFinder/static/js/members.js
@@ -1,11 +1,19 @@
 // members page menu
 
 function swapToS() {
-    document.getElementById("m").style.display = "none";
     document.getElementById("s").style.display = "block";
+    document.getElementById("u").style.display = "none";
+    document.getElementById("m").style.display = "none";
+}
+
+function swapToU() {
+    document.getElementById("s").style.display = "none";
+    document.getElementById("u").style.display = "block";
+    document.getElementById("m").style.display = "none";
 }
 
 function swapToM() {
-    document.getElementById("m").style.display = "block";
     document.getElementById("s").style.display = "none";
+    document.getElementById("u").style.display = "none";
+    document.getElementById("m").style.display = "block";
 }
